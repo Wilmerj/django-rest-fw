0d5f7706fd95919b4e4a695380b86dc37f4dcbe0
Wilmer Garzon <wilmerj1996@gmail.com>
2025-06-09 22:04:23 -0500

    feat: add docs to API with drf and swagger

--- patients/views.py
+++ patients/views.py
@@ -1,13 +1,12 @@
-from rest_framework.response import Response
 from .serializers import PatientSerializer
 from .models import Patient
-from rest_framework.decorators import api_view
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 
 class ListPatientsView(ListAPIView, CreateAPIView):
+    """
+    List all patients or create a new patient
+    """
     allowed_methods = ["GET", "POST"]
     serializer_class = PatientSerializer
     queryset = Patient.objects.all()
